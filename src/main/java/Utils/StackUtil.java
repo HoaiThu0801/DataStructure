e100f83c660ec38bcf9dbc3d3663833d07aee6b7
Hoai Thu <dangnguyenhoaithu08012000@gmail.com>
2022-08-15 00:57:39 +0700

    add infix to postfix function

--- src/main/java/Utils/StackUtil.java
+++ src/main/java/Utils/StackUtil.java
@@ -2,6 +2,7 @@ package Utils;
 
 import Linkedlist.SingleLinkedList;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class StackUtil {
@@ -58,5 +59,63 @@ public class StackUtil {
             }
         }
         return true;
+    }
+    public static String infixToPostfix(String s){
+        Stack stack = new Stack<Character>();
+        String res = "";
+        Character temp = null;
+        for (int i = 0; i < s.length(); i ++){
+            temp = s.charAt(i);
+            if (temp == '('){
+                stack.push(temp);
+            } else if (temp == ')') {
+                while(!stack.empty() && (Character)stack.peek() != '(' ){
+                    res += stack.pop();
+                }
+                stack.pop();
+            }else if (isOperator(s.charAt(i))) {
+                while(!stack.empty() && hasHigherPrec((Character) stack.peek(), temp)){
+                    res += stack.pop();
+                }
+                stack.push(temp);
+            }
+            else {
+                res += temp;
+            }
+        }
+        while(!stack.empty()){
+            res += stack.pop();
+        }
+        return res;
+    }
+    public static Boolean isOperator(Character c){
+        Character[] arrOperator = {'+', '-', '*', '/'};
+        for (Character i : arrOperator){
+            if (i == c){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static Boolean hasHigherPrec(Character operatorStack, Character operatorStr){
+        Character[] arrOperator = {'+', '-', '*', '/'};
+        int indexOfOSt = -1;
+        int indexOfOStr = -1;
+        for (int i = 0; i < arrOperator.length; i++){
+            if (arrOperator[i] == operatorStack){
+                indexOfOSt = i;
+            }
+            if (arrOperator[i] == operatorStr){
+                indexOfOStr = i;
+            }
+        }
+        if (indexOfOStr == -1 || indexOfOSt == -1){
+            return false;
+        }
+        if (indexOfOSt > indexOfOStr){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
--- src/test/java/Stack/StackUtilTest.java
+++ src/test/java/Stack/StackUtilTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.channels.SelectionKey;
 
-import static Utils.StackUtil.checkBalancedParentheses;
-import static Utils.StackUtil.reverse;
+import static Utils.StackUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StackUtilTest {
@@ -35,5 +34,11 @@ public class StackUtilTest {
         //assertFalse(checkBalancedParentheses("({{[[[]]}))"), "check parentheses went wrong");
         assertTrue(checkBalancedParentheses("{[(())]}"), "check parentheses went wrong");
     }
-
+    @Test
+    @DisplayName("Test convert infix to postfix function using stack")
+    void testInfixToPostfix(){
+        assertEquals("ABC*+", infixToPostfix("A+B*C"), "convert infix to postfix function something went wrong");
+        assertEquals("AB+C*", infixToPostfix("(A+B)*C"), "convert infix to postfix function something went wrong");
+        assertEquals("ABC*DE*-+", infixToPostfix("A+B*C-D*E"), "convert infix to postfix function something went wrong");
+    }
 }
